bce1aa5eb9903767a6d80eef83aba1a69f1d53bc
Josh Farish <josh_farish1@baylor.edu>
2021-10-13 15:46:39 -0500

    First day of pandas

---
+++ pandasSeries.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+grades = pd.Series([87, 100, 90])
+
+# print(grades)
+
+grades2 = pd.Series(98.6, range(3))
+
+# print(grades[0])
+
+# print(grades.describe())
+
+grades = pd.Series([87, 100, 94], index=["Wally", "Eva", "Sam"])
+
+grades = pd.Series({"Wally": 87, "Eva": 100, "Sam": 94})
+
+print(grades)
+
+print(grades["Eva"])
+
+print(grades.Wally)
+
+print(grades.dtype)
+
+print(grades.values)
+
+hardware = pd.Series(["Hammer", "Saw", "Wrench"])
+
+print(hardware)
+
+# calling string methods that apply to each element
+print(hardware.str.contains("a"))
+
+print(hardware.str.upper())
